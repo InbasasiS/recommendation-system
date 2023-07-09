48f54cf7cde367ea3dc28c6e50f3ec11960952f0
InbasasiS <inbasasis@gmail.com>
2023-07-09 08:06:52 +0530

    Clean Testcase

--- src/test/java/com/product/recommendation/service/RecommendationServiceTest.java
+++ src/test/java/com/product/recommendation/service/RecommendationServiceTest.java
@@ -40,107 +40,30 @@ class RecommendationServiceTest {
         List<Integer> themeIds = Arrays.asList(1001, 2001, 3001);
         Integer noOfProducts = 20;
 
-        List<Theme> userThemes = Arrays.asList(
-                Theme.builder().id(1001).name("Theme_A").TotalProducts(20).build(),
-                Theme.builder().id(2001).name("Theme_B").TotalProducts(5).build());
-        List<Theme> randomThemes = Arrays.asList(
-                Theme.builder().id(3001).name("Theme_C").TotalProducts(20).build());
-        List<Product> productsA = Arrays.asList(
-                Product.builder()
-                        .id(101)
-                        .name("Product_A_Theme_A")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build(),
-                Product.builder()
-                        .id(102)
-                        .name("Product_B_Theme_A")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build(),
-                Product.builder()
-                        .id(103)
-                        .name("Product_C_Theme_A")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build(),
-                Product.builder()
-                        .id(104)
-                        .name("Product_D_Theme_A")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build(),
-                Product.builder()
-                        .id(105)
-                        .name("Product_E_Theme_A")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build(),
-                Product.builder()
-                        .id(106)
-                        .name("Product_F_Theme_A")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build(),
-                Product.builder()
-                        .id(107)
-                        .name("Product_G_Theme_A")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build(),
-                Product.builder()
-                        .id(108)
-                        .name("Product_G_Theme_A")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build());
+        List<Theme> userThemes = getUserThemesMockData();
+        List<Theme> randomThemes = getRandomThemesMockData();
+        List<Product> productsA = getProductsAMockData(userThemes);
 
-        List<Product> productsB = Arrays.asList(
-                Product.builder()
-                        .id(201)
-                        .name("Product_A_Theme_B")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build(),
-                Product.builder()
-                        .id(202)
-                        .name("Product_B_Theme_B")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build(),
-                Product.builder()
-                        .id(203)
-                        .name("Product_C_Theme_B")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build(),
-                Product.builder()
-                        .id(204)
-                        .name("Product_D_Theme_B")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build(),
-                Product.builder()
-                        .id(205)
-                        .name("Product_E_Theme_B")
-                        .price(300F)
-                        .theme(userThemes.get(0))
-                        .weigtage(0.5F)
-                        .build());
+        List<Product> productsB = getProductsBMockData(userThemes);
 
-        List<Product> productsC = Arrays.asList(
+        List<Product> productsC = getProductsCMockData(userThemes);
+
+        when(themeDaoService.fetchThemeByIds(anyList())).thenReturn(new ArrayList<>(userThemes));
+        when(themeDaoService.collectRandomThemes(anyList(), anyInt())).thenReturn(new ArrayList<>(randomThemes));
+
+        when(productDtoService.fetchProductsByIdsAndCount(1001, 11)).thenReturn(productsA);
+        when(productDtoService.fetchProductsByIdsAndCount(2001, 5)).thenReturn(productsB);
+        when(productDtoService.fetchProductsByIdsAndCount(3001, 4)).thenReturn(productsC);
+        recommendationService.recommendProducts(userId, themeIds, noOfProducts);
+
+        // This testcase validates the fetchProductsByIdsAndCount method is called with the correct args
+        verify(productDtoService).fetchProductsByIdsAndCount(1001, 11);
+        verify(productDtoService).fetchProductsByIdsAndCount(2001, 5);
+        verify(productDtoService).fetchProductsByIdsAndCount(3001, 4);
+    }
+
+    private List<Product> getProductsCMockData(List<Theme> userThemes) {
+        return Arrays.asList(
                 Product.builder()
                         .id(301)
                         .name("Product_A_Theme_C")
@@ -211,18 +134,115 @@ class RecommendationServiceTest {
                         .theme(userThemes.get(0))
                         .weigtage(0.5F)
                         .build());
+    }
 
-        when(themeDaoService.fetchThemeByIds(anyList())).thenReturn(new ArrayList<>(userThemes));
-        when(themeDaoService.collectRandomThemes(anyList(), anyInt())).thenReturn(new ArrayList<>(randomThemes));
+    private List<Product> getProductsBMockData(List<Theme> userThemes) {
+        return Arrays.asList(
+                Product.builder()
+                        .id(201)
+                        .name("Product_A_Theme_B")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build(),
+                Product.builder()
+                        .id(202)
+                        .name("Product_B_Theme_B")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build(),
+                Product.builder()
+                        .id(203)
+                        .name("Product_C_Theme_B")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build(),
+                Product.builder()
+                        .id(204)
+                        .name("Product_D_Theme_B")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build(),
+                Product.builder()
+                        .id(205)
+                        .name("Product_E_Theme_B")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build());
+    }
 
-        when(productDtoService.fetchProductsByIdsAndCount(1001, 11)).thenReturn(productsA);
-        when(productDtoService.fetchProductsByIdsAndCount(2001, 5)).thenReturn(productsB);
-        when(productDtoService.fetchProductsByIdsAndCount(3001, 4)).thenReturn(productsC);
-        recommendationService.recommendProducts(userId, themeIds, noOfProducts);
+    private List<Product> getProductsAMockData(List<Theme> userThemes) {
+        return Arrays.asList(
+                Product.builder()
+                        .id(101)
+                        .name("Product_A_Theme_A")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build(),
+                Product.builder()
+                        .id(102)
+                        .name("Product_B_Theme_A")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build(),
+                Product.builder()
+                        .id(103)
+                        .name("Product_C_Theme_A")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build(),
+                Product.builder()
+                        .id(104)
+                        .name("Product_D_Theme_A")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build(),
+                Product.builder()
+                        .id(105)
+                        .name("Product_E_Theme_A")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build(),
+                Product.builder()
+                        .id(106)
+                        .name("Product_F_Theme_A")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build(),
+                Product.builder()
+                        .id(107)
+                        .name("Product_G_Theme_A")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build(),
+                Product.builder()
+                        .id(108)
+                        .name("Product_G_Theme_A")
+                        .price(300F)
+                        .theme(userThemes.get(0))
+                        .weigtage(0.5F)
+                        .build());
+    }
 
-        // This testcase validates the fetchProductsByIdsAndCount method is called with the correct args
-        verify(productDtoService).fetchProductsByIdsAndCount(1001, 11);
-        verify(productDtoService).fetchProductsByIdsAndCount(2001, 5);
-        verify(productDtoService).fetchProductsByIdsAndCount(3001, 4);
+    private List<Theme> getRandomThemesMockData() {
+        return Arrays.asList(
+                Theme.builder().id(3001).name("Theme_C").TotalProducts(20).build());
+    }
+
+    private List<Theme> getUserThemesMockData() {
+        return Arrays.asList(
+                Theme.builder().id(1001).name("Theme_A").TotalProducts(20).build(),
+                Theme.builder().id(2001).name("Theme_B").TotalProducts(5).build());
     }
 }
